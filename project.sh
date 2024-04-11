9c58ff35285900ce8dfa17ba078b85050ffad2c9
YouriyPaluch <korhyn@gmail.com>
2024-04-11 21:00:53 +0300

    fix project.sh

--- project.sh
+++ project.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
-
-#install=false
-#
-#while [[ "$#" -gt 0 ]]; do
-#    case $1 in
-#        -i|--install) install=true; shift ;;
-##        -u|--uglify) uglify=1 ;;
-#        *) echo "Unknown parameter passed: $1"; exit 1 ;;
-#    esac
-#    shift
-#done
-
 if test -f "${WORKDIR}/.env"; then
       echo 'Run';
       docker-compose --env-file .env.local up;
@@ -18,12 +6,3 @@ if test -f "${WORKDIR}/.env"; then
       echo 'Installation';
       docker-compose --env-file .env.install up --build;
 fi
-
-#if [[ "$install" == true ]]; then
-#      echo 'Installation';
-#      docker-compose --env-file .env.install up --build;
-#    else
-#      echo 'Run';
-#      docker-compose --env-file .env.local up;
-#fi
-
