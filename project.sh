90638a4fdb7a10b6c516567671b577cde2ab72cd
YouriyPaluch <90302380+YouriyPaluch@users.noreply.github.com>
2024-04-20 08:04:35 +0300

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
-if test -f "${WORKDIR}/.env"; then
+if test -f .env; then
       echo 'Run';
-      docker-compose --env-file .env.local up;
+#      docker-compose --env-file .env.local up;
     else
       echo 'Installation';
+      if test -f docker-compose.yaml; then
+        rm docker-compose.yaml;
+      fi
+      cp docker-compose.yaml.example docker-compose.yaml;
       docker-compose --env-file .env.install up --build;
+      chmod 777 docker-compose.yaml;
+      rm docker-compose.yaml;
+      sed '3,13d' docker-compose.yaml.example > docker-compose.yaml;
 fi
